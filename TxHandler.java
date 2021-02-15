ed8255e0cd3d9a382f77413ae7b614138e953c64
whitejeric <whitejeric@gmail.com>
2021-02-14 18:15:53 -0800

    Update TxHandler.java

--- TxHandler.java
+++ TxHandler.java
@@ -37,11 +37,11 @@ public class TxHandler {
     	Transaction.Input tempIn;
     	Transaction.Output tempOut;
 
-    	 // Solving (1), (2), (3)
+    	// Solving (1), (2), (3)
     	for (int i=0; i < inputs.size(); i++) {
     		tempIn = inputs.get(i);
     		tempUTXO = new UTXO (tempIn.prevTxHash, tempIn.outputIndex); //grabbing the UTXO from which this input stems
-    		if (!tempPool.contains(tempUTXO)) return false;	//UTXO not in pool => null claim	OR UTXO already claimed in transaction
+    		if (!tempPool.contains(tempUTXO)) return false;	//UTXO not in pool => null claim OR UTXO already claimed in transaction
     		tempOut = tempPool.getTxOutput(tempUTXO); // the address and index of the prev transaction that should lead into our input
     		if (tempOut == null) return false;
     		if (!Crypto.verifySignature(tempOut.address, tx.getRawDataToSign(i), tempIn.signature)) return false;
@@ -49,7 +49,7 @@ public class TxHandler {
     		tempPool.removeUTXO(tempUTXO);
     	}
 
-    	 // Solving (4), (5)
+    	// Solving (4), (5)
     	int j = 0;
     	while (j < outputs.size()){
     		tempOut = outputs.get(j);
@@ -81,7 +81,7 @@ public class TxHandler {
 
     	/**
     	 * Note:
-			 * Since some transactions are dependant on other transactions in possibleTxs, I tried
+	 * Since some transactions are dependant on other transactions in possibleTxs, I tried
     	 * re-verifying any transactions that failed on the first verification loop.
     	 * Pretty sure this idea didn't work but I ran out of time
     	 */
@@ -95,7 +95,6 @@ public class TxHandler {
 
     	Transaction addTxs[] = new Transaction[this.validTxs.size()];
     	return this.validTxs.toArray(addTxs);
-
     }
 
     /**
@@ -104,22 +103,23 @@ public class TxHandler {
      */
 
     private void addToPool(Transaction tx) {
-	   	tx.finalize();
-	   	// First removing the input UTXO as it is spent
-	   	ArrayList<Transaction.Input> inputs = tx.getInputs();
-	   	UTXO t;
-			for (Transaction.Input input : inputs) {
-				t = new UTXO(input.prevTxHash, input.outputIndex);
-				if (transLedger.contains(t)) this.transLedger.removeUTXO(t);
-			}
-			// Then adding the new UTXO
-			int i = 0;	//serial num
-			ArrayList<Transaction.Output> outputs = tx.getOutputs();
-			for (Transaction.Output output: outputs) {
-				t = new UTXO(tx.getHash(), i);
-				if (!transLedger.contains(t)) this.transLedger.addUTXO(t, output);
-				i++;
-			}
+	tx.finalize();
+	// First removing the input UTXO as it is spent
+	ArrayList<Transaction.Input> inputs = tx.getInputs();
+	UTXO t;
+	for (Transaction.Input input : inputs) {
+		t = new UTXO(input.prevTxHash, input.outputIndex);
+		if (transLedger.contains(t)) this.transLedger.removeUTXO(t);
+	}
+	    
+	// Then adding the new UTXO
+	int i = 0; //serial num
+	ArrayList<Transaction.Output> outputs = tx.getOutputs();
+	for (Transaction.Output output: outputs) {
+		t = new UTXO(tx.getHash(), i);
+		if (!transLedger.contains(t)) this.transLedger.addUTXO(t, output);
+		i++;
+	}
    }
 
 }
